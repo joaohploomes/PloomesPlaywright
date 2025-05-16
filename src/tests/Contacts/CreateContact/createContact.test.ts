f4f3e2e76d64bb49b953ad9028cf3ce591dfaeef
MayconBrandaleroPloomes <maycon.brandalero@ploomes.com>
2025-05-16 12:29:09 -0300

    fix: contact test

--- src/tests/Contacts/CreateContact/createContact.test.ts
+++ src/tests/Contacts/CreateContact/createContact.test.ts
@@ -3,10 +3,6 @@ import { expect, test } from "@playwright/test";
 import generateMockedContact from "../mockedDataContact/mockedDataContact";
 import type { IContact } from "@schemas";
 import { generateMultipleItens, deleteMultipleItens } from "@lib";
-<<<<<<< HEAD
-
-=======
->>>>>>> db8e2aba6008b19c1a8ed44f8e088c8d0cf5614f
 
 test.describe("Contact tests", () => {
 	test("Create a Contact", async () => {
@@ -22,23 +18,11 @@ test.describe("Contact tests", () => {
 	test("Find all Contacts", async () => {
 		const contactController = new ContactController();
 	
-<<<<<<< HEAD
-		const [
-			contact1,
-			contact2,
-			contact3
-		] = await generateMultipleItens<IContact>(contactController.createContact, generateMockedContact, 3);
-=======
 		await generateMultipleItens<IContact>(contactController.createContact.bind(ContactController), generateMockedContact, 10);
->>>>>>> db8e2aba6008b19c1a8ed44f8e088c8d0cf5614f
 
 		const contacts = await contactController.findAllContacts();
 		expect(contacts).toBeDefined();
 
-<<<<<<< HEAD
-		await deleteMultipleItens<IContact>(contactController.deleteContact, [contact1, contact2, contact3]);
-=======
 		await deleteMultipleItens<IContact>(contactController.deleteContact, [contacts]);
->>>>>>> db8e2aba6008b19c1a8ed44f8e088c8d0cf5614f
 	});
 });
